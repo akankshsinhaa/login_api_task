cb1edc402f151b4fc712d5d9fce3a3855743e514
Panav <panavsinha15@gmail.com>
2021-11-29 13:24:10 +0530

    Second Screen Added

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(myApp);
+}
+
+MaterialApp myApp = const MaterialApp(
+  home: Login(),
+  debugShowCheckedModeBanner: false,
+);
